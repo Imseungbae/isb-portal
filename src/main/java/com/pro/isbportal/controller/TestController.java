f83dd2cb914a4e9c08ae8c0d9048d44ae12c5696
isb <tmdqo9@gmail.com>
2022-02-07 20:25:12 +0900

    Add task print(), Edit testURL

--- src/main/java/com/pro/isbportal/controller/TestController.java
+++ src/main/java/com/pro/isbportal/controller/TestController.java
@@ -4,12 +4,14 @@ import com.pro.isbportal.service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/isb-portal")
 @Slf4j
 public class TestController {
     private final TestService testService;
